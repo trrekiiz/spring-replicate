8f6adac78ea3e9404c46d66a7750e5169fb15309
Kunchai Sodhom <kunchai.sdhm@gmail.com>
2018-04-10 12:08:41 +0700

    Add spring-batch-context for call file yml to run this job with sql statement

--- src/main/java/java/th/co/ipassion/replicate/main/MainApplication.java
+++ src/main/java/java/th/co/ipassion/replicate/main/MainApplication.java
@@ -2,12 +2,31 @@ package java.th.co.ipassion.replicate.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobExecutionException;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.scheduling.annotation.Scheduled;
 
 @SpringBootApplication
 public class MainApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MainApplication.class, args);
-        System.out.println("Hello World");
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring-batch-context.xml");
+
+        JobLauncher jobLauncher = (JobLauncher) context.getBean("jobLauncher");
+        Job job = (Job) context.getBean("migrationJob");
+
+        try {
+            JobExecution execution = jobLauncher.run(job, new JobParameters());
+            System.out.println("Job Exit Status : "+ execution.getStatus());
+
+        } catch (JobExecutionException e) {
+            System.out.println("Migration job failed");
+            e.printStackTrace();
+        }
     }
 }
